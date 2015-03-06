d5fd507ebb4f599321ad06e3fbb107ce537c6646
Thomas Maximini <tmaximini@gmail.com>
2015-03-06 11:30:47 +0100

    include gitignore

--- app/index.js
+++ app/index.js
@@ -80,6 +80,10 @@ module.exports = yeoman.generators.Base.extend({
         this.destinationPath('.editorconfig')
       );
       this.fs.copy(
+        this.templatePath('gitignore'),
+        this.destinationPath('.gitignore')
+      );
+      this.fs.copy(
         this.templatePath('jshintrc'),
         this.destinationPath('.jshintrc')
       );

fb26af913fc1e882b0de7332983f1f67204b7de5
Faisal Kurdi <fhkurdi@icloud.com>
2015-08-04 14:04:51 -0700

    clearer

--- app/templates/_gulpfile.js
+++ app/templates/_gulpfile.js
@@ -67,26 +67,26 @@ gulp.task('styles', function() {
 
   var options = build ? { style: 'compressed' } : { style: 'expanded' };
 
-  // var sassStream = gulp.src('app/styles/main.scss')
-  //   .pipe(plugins.sass(options))
-  //   .on('error', function(err) {
-  //     console.log('err: ', err);
-  //     beep();
-  //   });
+  var sassStream = gulp.src('app/styles/main.scss')
+    .pipe(plugins.sass(options))
+    .on('error', function(err) {
+      console.log('err: ', err);
+      beep();
+    });
 
 
   // build ionic css dynamically to support custom themes
-  var ionicStream = gulp.src('bower_components/ionic/scss/ionic.scss')
-    // .pipe(plugins.cached('ionic-styles'))
-    .pipe(plugins.sass(options))
-    // cache and remember ionic .scss in order to cut down re-compile time
-    // .pipe(plugins.remember('ionic-styles'))
-    .on('error', function(err) {
-        console.log('err: ', err);
-        beep();
-      });
+  // var ionicStream = gulp.src('bower_components/ionic/scss/ionic.scss')
+  //   .pipe(plugins.cached('ionic-styles'))
+  //   .pipe(plugins.sass(options))
+  //   // cache and remember ionic .scss in order to cut down re-compile time
+  //   .pipe(plugins.remember('ionic-styles'))
+  //   .on('error', function(err) {
+  //       console.log('err: ', err);
+  //       beep();
+  //     });
 
-  return streamqueue({ objectMode: true }, ionicStream)
+  return streamqueue({ objectMode: true }, sassStream)
     .pipe(plugins.autoprefixer('last 1 Chrome version', 'last 3 iOS versions', 'last 3 Android versions'))
     .pipe(plugins.concat('main.css'))
     .pipe(plugins.if(build, plugins.stripCssComments()))

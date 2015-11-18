3583d9fe566cc345fcadafed6762234d12381d40
Mark Doeswijk <mark@springtree.nl>
2015-11-18 10:55:33 +0100

    Added source files for browserify based project

---
+++ app/templates/src/controllers/mainController.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name CTASpore.controller:MainController
+ * @description
+ * # MainController
+ */
+module.exports = [
+    '$scope',
+
+    function( $scope )
+    {
+        // do something with $scope
+    }
+];
